ff7ce7705de945a94d936d2ddf0660ebcd28beae
yusengo <yusengo@163.com>
2017-07-06 19:00:21 +0800

    20170706ok

---
+++ Demo/testCV.py
@@ -0,0 +1,40 @@
+"""
+from PIL import Image
+from pylab import *
+
+im = array(Image.open('wing.jpg'))
+imshow(im)
+
+x = [100,200,400,400]
+y = [100,200,200,500]
+
+plot(x,y,'r*')
+plot(x[:2],y[:2],'w')
+
+title('plotting:"wing.jpg"')
+axis('off')
+show()
+"""
+"""
+from PIL import Image
+from pylab import *
+
+im = array(Image.open('wing.jpg').convert('L'))
+figure()
+gray()
+contour(im,origin='image')
+axis('equal')
+figure()
+hist(im.flatten(),128)
+show()
+"""
+from PIL import Image
+from pylab import *
+
+im = array(Image.open('wing.jpg'))
+imshow(im)
+
+print 'please click 3 points'
+x = ginput(3)
+print 'you clicked :',x
+show()
